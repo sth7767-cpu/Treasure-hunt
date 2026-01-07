bca9af10266a07ad9495262834e2a9b921481b22
thdgnswjd510-spec <thdgnswjd510@gmail.com>
2026-01-07 11:56:49 +0900

    Update player.py

--- player.py
+++ player.py
@@ -1,14 +1,53 @@
-def car_frame():
+if __name__ != "__main__":
 
-    exhaust = " }"
+    def car_frame():
 
-    car = [
-        "  .==.  ",
-        " //__\\\\ ",
-        "[| __ |]",
-        " | || | ",
-        "[| || |]",
-        " \\____/ ",
-        "  " + exhaust + exhaust + "   "
-    ]
-    print('\n'.join(car))
+        exhaust = " }"
+
+        car = [
+            "  .==.  ",
+            " //__\\\\ ",
+            "[| __ |]",
+            " | || | ",
+            "[| || |]",
+            " \\____/ ",
+            "  " + exhaust + exhaust + "   "
+        ]
+        return '\n'.join(car)
+
+
+    def get_rock():
+        """ 둥근 바위 모양 """
+        rock = [
+            "   .---.   ",
+            "  /     \\  ",
+            " (  rock ) ",
+            "  `-----'  "
+        ]
+        return '\n'.join(rock)
+
+
+    def get_cone():
+        """ 공사중 라바콘(고깔) 모양 """
+        cone = [
+            "     ^     ",
+            "    / \\    ",
+            "   / _ \\   ",
+            "  /_____\\  ",
+            " [_______] "
+        ]
+        return '\n'.join(cone)
+
+
+    def get_wall():
+        """ 가로로 긴 벽돌담 모양 """
+        wall = [
+            "|WARNING|",
+            "|=======|",
+            "|_______|"
+        ]
+        return '\n'.join(wall)
+
+
+
+
