ff56e6ad4f6b5c1725066a236bf051189a718e02
thdgnswjd510-spec <thdgnswjd510@gmail.com>
2026-01-07 11:18:10 +0900

    Update player.py

---
+++ player.py
@@ -0,0 +1,14 @@
+def car_frame():
+
+    exhaust = " }"
+
+    car = [
+        "  .==.  ",
+        " //__\\\\ ",
+        "[| __ |]",
+        " | || | ",
+        "[| || |]",
+        " \\____/ ",
+        "  " + exhaust + exhaust + "   "
+    ]
+    print('\n'.join(car))
